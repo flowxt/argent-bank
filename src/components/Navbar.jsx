dc9083dbf2bd2abd2036e8e3b5e4d1ad7417e98f
florian <florianbarjon@icloud.com>
2025-01-14 13:34:22 +0100

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,43 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { logoutUser } from '../actions/authActions';
 import logo from '../assets/images/argentBankLogo.png';
 
-
 const Navbar = () => {
-    
-    return (
-        <nav className='main-nav'>
-            <Link className='main-nav-logo' to ='/'>
-            <img className='main-nav-logo-image' src={logo} alt='Argent Bank Logo'/>
-            <h1 className='sr-only'>Argent Bank</h1>
-            </Link>
-      
-        <div>
-            <Link className='main-nav-item' to="/sign-in">
-            <i className="fa fa-user-circle"></i>
-            Sign In
-            </Link>
-        </div>
-        </nav>
-    );
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  const firstName = useSelector((state) => state.auth.firstName);
+  const lastName = useSelector((state) => state.auth.lastName);
+
+  const handleLogout = () => {
+    dispatch(logoutUser());
+  };
+
+  return (
+    <nav className='main-nav'>
+      <Link className='main-nav-logo' to='/'>
+        <img className='main-nav-logo-image' src={logo} alt='Argent Bank Logo'/>
+        <h1 className='sr-only'>Argent Bank</h1>
+      </Link>
+      <div>
+        {isAuthenticated ? (
+          <>
+            <span className='main-nav-item'>
+              {firstName} {lastName}
+            </span>
+            <button className='main-nav-item' onClick={handleLogout}>
+              <i className="fa fa-sign-out"></i> Sign Out
+            </button>
+          </>
+        ) : (
+          <Link className='main-nav-item' to="/sign-in">
+            <i className="fa fa-user-circle"></i> Sign In
+          </Link>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
